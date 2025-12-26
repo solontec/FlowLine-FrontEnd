8c9eed91cec6470051a1a13676683c17d2fd16c7
solontec <guisolon.tec@gmail.com>
2025-12-26 17:07:23 -0300

    Começando a adicionar as rotas para consumir a API do eduardo

---
+++ app/(auth)/Cadastro/page.tsx
@@ -0,0 +1,7 @@
+function cadastro(){
+    return(
+        <h1>This is cadastro</h1>
+    )
+}
+
+export default cadastro
---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+function login(){
+    return(
+        <h1>This is login</h1>
+    )
+}
+
+export default login
